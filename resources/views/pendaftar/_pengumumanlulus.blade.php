77846f73f2e6c3b7877f20a6c767d9d15256fa52
Kevin <kevin.zhong94@gmail.com>
2015-04-17 01:20:26 +0700

    status kelulusan

--- resources/views/pendaftar/_pengumumanlulus.blade.php
+++ resources/views/pendaftar/_pengumumanlulus.blade.php
@@ -3,10 +3,12 @@
         <div class="add-margin-top-bottom">
             <h1 class="text-center">Pengumuman Ujian Masuk</h1>
         </div>
-        <h3 class="text-center">Selamat, Anda diterima sebagai siswa Pribadi BBS Bandung</h3>
-        <p class="text-center">Lakukan pendaftaran ulang sesuai dengan jadwal yang telah ditentukan.</p>
-        <p class="text-center">Bawalah berkas-berkas yang diperlukan pada saat pendaftaran ulang.</p>
-
-        <h3 class="text-center">Maaf, Anda tidak lulus Ujian Masuk Pribadi BBS Bandung.</h3>
+        @if ($dataSiswa->status_kelulusan==1)
+	        <h3 class="text-center">Selamat, Anda diterima sebagai siswa Pribadi BBS Bandung</h3>
+	        <p class="text-center">Lakukan pendaftaran ulang sesuai dengan jadwal yang telah ditentukan.</p>
+	        <p class="text-center">Bawalah berkas-berkas yang diperlukan pada saat pendaftaran ulang.</p>
+        @else
+ 	       <h3 class="text-center">Maaf, Anda tidak lulus Ujian Masuk Pribadi BBS Bandung.</h3>
+        @endif
     </div>
 </div>
